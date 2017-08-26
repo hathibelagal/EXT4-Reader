9ce644cdf3a5e67d88e5c659c243d31d2d281103
Hathibelagal <mlrdae@gmail.com>
2017-08-27 04:02:19 +0530

    Simple file recovery

--- reader.py
+++ reader.py
@@ -30,7 +30,7 @@ class Ext4FileSystemReader:
         self.file_system = False
         self.file_system_size = False
 
-        opts, _ = getopt(args, 'f:s:')
+        opts, _ = getopt(args, 'f:s:r:')
         for option, value in opts:
             if option == '-f':
                 self.file_system = value
@@ -38,11 +38,16 @@ class Ext4FileSystemReader:
             if option == '-s':
                 self.file_system_size = long(value)
                 print("Using %d as the size of the file system" % self.file_system_size)
+            if option == '-r':
+                if value == 'yes':
+                    self.recover_files = True
+                else:
+                    self.recover_files = False
 
         if self.file_system and self.file_system_size:
             self.read_first_block()
         else:
-            print("Usage: -f <filesystem> -s <filesystem_size>")
+            print("Usage: -f <filesystem> -s <filesystem_size> -r <yes|no>")
             print("Note: Superuser privileges might be required")
             exit(1)
                         
@@ -55,7 +60,7 @@ class Ext4FileSystemReader:
             inode_details = struct.unpack('13I',super_block[0:13 * 4])
             diagnostic_details = struct.unpack('6H',super_block[52:52+6*2])
 
-            print "File system details:"
+            print("File system details:")
             self.n_inodes = inode_details[0]
             self.n_blocks = inode_details[1]
             self.block_size = 2 ** (10 + inode_details[6])
@@ -63,7 +68,6 @@ class Ext4FileSystemReader:
             self.block_group_size = self.block_size * self.blocks_per_group
             self.inodes_per_group = inode_details[10]
             self.number_of_block_groups = self.file_system_size / self.block_group_size + 1
-            self.group_descriptor_size = 32
             print("  Number of inodes: %d" % self.n_inodes)
             print("  Number of blocks: %d" % self.n_blocks)
             print("  Size of each block: %d bytes" % self.block_size)
@@ -99,6 +103,7 @@ class Ext4FileSystemReader:
                 print("  Has reserved blocks for expansion: True")
                 self.reserved_blocks = True
             if incompat_details[1] & 0x80 == 0:
+                self.group_descriptor_size = 32
                 print("  Size of block group descriptor: 32 bytes")
             else:
                 print("  Size of block group descriptor: >32 bytes")
@@ -152,12 +157,12 @@ class Ext4FileSystemReader:
     def read_all_inodes(self):
         block_group_number = 0
         inode_number = 1
-        with open(self.file_system) as f:
+        with open(self.file_system, "rb") as f:
             for block_group_number in xrange(self.number_of_block_groups):
 
                 #Read only initialized inode tables
                 if self.inode_tables[block_group_number]['flags'] != 4:
-                    print("Inode table uninitialized at block group %d" % block_group_number)
+                    print("Inode table uninitialized in block group %d" % block_group_number)
                     continue
 
                 print("Reading inode table of block group %d"%(block_group_number))
@@ -175,13 +180,62 @@ class Ext4FileSystemReader:
                     number_of_512_blocks = struct.unpack('I', current_inode[0x1C: 0x1C + 1 * 4])[0]
                     uses_extents = (struct.unpack('I', current_inode[0x20: 0x20 + 1 * 4])[0] & 0x80000) != 0
                     if current_file_size > 0:
-                        print "%d -> (Permissions: %x, Size: %d, Type: %s, sBlocks: %d, Extents: %s)" % (inode_number, 
-                                current_inode_meta_data[0], 
-                                current_inode_meta_data[2], 
-                                current_file_type, 
-                                number_of_512_blocks, 
-                                uses_extents)
+                        print("%d -> (Permissions: %x, Size: %d, Type: %s, sBlocks: %d, Extents: %s)" % (inode_number, 
+                                        current_inode_meta_data[0], 
+                                        current_inode_meta_data[2], 
+                                        current_file_type, 
+                                        number_of_512_blocks, 
+                                        uses_extents))
+                        if (self.recover_files and 
+                                    current_file_type == 'Regular file' and 
+                                    inode_number >= self.first_usable_inode):
+                            self.recover(current_inode, current_file_type, uses_extents, 
+                                         number_of_512_blocks, current_file_size) 
                     inode_number += 1
+
+    def recover(self, inode, file_type, uses_extents, number_of_512_blocks, current_file_size):
+        i_block = inode[0x28:0x28 + 60]
+        if uses_extents:
+            extent_header = struct.unpack('4HI', i_block[:12])
+            if extent_header[0] != 0xF30A:
+                print("Error in extent header: %x" % extent_header[0])
+                return
+            else:
+                n_entries = extent_header[1]
+                depth = extent_header[3]
+                if depth == 0 and n_entries > 0:
+                    self.recover_from_leaf_node(i_block, number_of_512_blocks, 
+                                                current_file_size, n_entries)
+
+    def recover_from_leaf_node(self, i_block, number_of_512_blocks, current_file_size, n_entries):
+        with open(self.file_system, "rb") as f:
+            starting_point = 12
+            total_bytes_remaining = current_file_size
+            for entry in xrange(n_entries):
+                leaf = struct.unpack('I2HI',i_block[starting_point:starting_point+12])
+                block_number_lo = leaf[3]
+                block_number_hi = leaf[2]
+                block_number = block_number_hi << 32 | block_number_lo
+                maximum_readable_bytes = leaf[1] * self.block_size
+                if block_number < self.n_blocks:
+                    print("Fetching block: %d" % block_number)
+                    f.seek(block_number * self.block_size)
+                    total_bytes_read = 0
+                    for i in xrange(number_of_512_blocks):
+                        bytes_to_read = 512
+                        if bytes_to_read > total_bytes_remaining:
+                            bytes_to_read = total_bytes_remaining
+                        print("DATA (%d, %d)\n-------------" % (entry, i))
+                        print(f.read(bytes_to_read))
+                        total_bytes_remaining -= bytes_to_read
+                        total_bytes_read += bytes_to_read
+                        if total_bytes_remaining <= 0:
+                            break
+                        if total_bytes_read >= maximum_readable_bytes:
+                            break
+                else:
+                    print("Bad block number found: %d" % block_number)
+                starting_point+=12
 
 if __name__ == "__main__":
     fs = Ext4FileSystemReader(sys.argv[1:])
